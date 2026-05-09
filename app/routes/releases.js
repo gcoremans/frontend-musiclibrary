11e6bae3eee25b0a0017cdd4223b19a4ed53f606
Gilles Coremans <gilles.coremans@gmail.com>
2026-05-09 19:22:18 +0200

    Add basic placeholder pages

--- app/routes/releases.js
+++ app/routes/releases.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ReleasesRoute extends Route {}
+export default class ReleasesRoute extends Route {
+    @service store;
+
+    model() {
+        return this.store.findAll('release');
+    }
+}
--- app/routes/tracks.js
+++ app/routes/tracks.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class TracksRoute extends Route {}
+export default class TracksRoute extends Route {
+    @service store;
+
+    model() {
+        return this.store.findAll('track');
+    }
+}
