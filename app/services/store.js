f0e4f4f12a01e5101b366acf068a85ab618e174f
Gilles Coremans <gilles.coremans@redpencil.io>
2026-05-21 17:05:23 +0200

    Enable legacy requests on the store

--- app/services/store.js
+++ app/services/store.js
@@ -2,6 +2,7 @@ import { useLegacyStore } from '@warp-drive/legacy';
 import { JSONAPICache } from '@warp-drive/json-api';
 
 const Store = useLegacyStore({
+  legacyRequests: true,
   linksMode: false,
   cache: JSONAPICache,
   handlers: [
